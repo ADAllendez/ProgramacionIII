1f1e8bce617b27a8b97363ffb4433a98bb7c7497
ADAllendez <alejo.diaz80.ad@gmail.com>
2025-05-23 20:31:17 -0300

    Agregando ejericio4

--- Ejercicios/Ejercicio4/script.js
+++ Ejercicios/Ejercicio4/script.js
@@ -1,1 +1,11 @@
-let resultado = " ";
+function calcular(){
+    const altura = parseFloat(document.getElementById("altura").value);
+    const peso = parseFloat(document.getElementById("peso").value);
+
+    if (isNaN(altura) || isNaN(peso) || altura <= 0 || peso <= 0) {
+        alert("Los valores no deben estar vacios");
+        return;
+    }
+    const imc = peso / (altura*altura);
+    alert(`Tu IMC es ${imc.toFixed(2)}`);
+}
